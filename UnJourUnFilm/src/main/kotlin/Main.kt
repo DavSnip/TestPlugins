010f39af9d98e2ae8a0dda570f315b00dfccf173
DavSnip <davidconeggo@gmail.com>
2026-04-23 09:59:14 +0200

    Update Main.kt

--- UnJourUnFilm/src/main/kotlin/Main.kt
+++ UnJourUnFilm/src/main/kotlin/Main.kt
@@ -1,8 +1,11 @@
 import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.TvType
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 
+@CloudstreamPlugin
 class UnjourUnfilm : MainAPI() {
     override var name = "UnjourUnfilm"
     override var mainUrl = "https://1jour1film0426c.site"
+    override var lang = "fr"
     override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
 }
