50bf07a537d5854223c7cc588617e4ab222a9e27
DavSnip <davidconeggo@gmail.com>
2026-04-23 09:22:17 +0200

    Delete Coflix/Main.kt

--- Coflix/Main.kt
+++
@@ -1,8 +0,0 @@
-import com.lagradost.cloudstream3.MainAPI
-import com.lagradost.cloudstream3.TvType
-
-class Coflix : MainAPI() {
-    override var name = "Coflix"
-    override var mainUrl = "https://coflix.wales"
-    override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
-}
