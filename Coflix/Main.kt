2b2731a2871bd6517fd4857cfb52668c2f2da064
DavSnip <davidconeggo@gmail.com>
2026-04-23 08:43:49 +0200

    Update Main.kt

--- Coflix/Main.kt
+++ Coflix/Main.kt
@@ -1,7 +1,7 @@
 import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.TvType
 
-class Papadustream : MainAPI() {
+class Coflix : MainAPI() {
     override var name = "Coflix"
     override var mainUrl = "https://coflix.wales"
     override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
