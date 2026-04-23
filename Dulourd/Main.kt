cf4753e769aa2a17f4f6b085cb061d27c35a9f9a
DavSnip <davidconeggo@gmail.com>
2026-04-23 08:40:07 +0200

    Update Main.kt

--- Dulourd/Main.kt
+++ Dulourd/Main.kt
@@ -1,1 +1,8 @@
+import com.lagradost.cloudstream3.MainAPI
+import com.lagradost.cloudstream3.TvType
 
+class Papadustream : MainAPI() {
+    override var name = "Dulourd"
+    override var mainUrl = "https://dulourd.boo"
+    override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
+}
