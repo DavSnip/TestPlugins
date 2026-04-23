2edc50c5632fbda06de4bc7344d3926153f29ea1
DavSnip <davidconeggo@gmail.com>
2026-04-23 10:19:39 +0200

    Update Main.kt

--- Dulourd/src/main/kotlin/Main.kt
+++ Dulourd/src/main/kotlin/Main.kt
@@ -5,7 +5,7 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 @CloudstreamPlugin
 class Dulourd : MainAPI() {
     override var name = "Dulourd"
-    override var mainUrl = "https://dulourd.boo"
+    override var mainUrl = "https://dulourd.hair"
     override var lang = "fr"
     override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
 }
