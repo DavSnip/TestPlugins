b33103d52ec8580bd630aa7f7bcdbd46c34f59e1
DavSnip <davidconeggo@gmail.com>
2026-04-23 09:24:06 +0200

    Delete Dulourd/Main.kt

--- Dulourd/Main.kt
+++
@@ -1,8 +0,0 @@
-import com.lagradost.cloudstream3.MainAPI
-import com.lagradost.cloudstream3.TvType
-
-class Dulourd : MainAPI() {
-    override var name = "Dulourd"
-    override var mainUrl = "https://dulourd.boo"
-    override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
-}
