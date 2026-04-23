2d6b9e213b1bb0a56df2cd1b3e746aea3e0557f1
DavSnip <davidconeggo@gmail.com>
2026-04-23 09:57:03 +0200

    Update Main.kt

--- Coflix/src/main/kotlin/Main.kt
+++ Coflix/src/main/kotlin/Main.kt
@@ -1,8 +1,11 @@
 import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.TvType
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 
+@CloudstreamPlugin
 class Coflix : MainAPI() {
     override var name = "Coflix"
     override var mainUrl = "https://coflix.wales"
+    override var lang = "fr"
     override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
 }
