51105668ea9f2b0a6f0e48e3df41524eff02e719
DavSnip <davidconeggo@gmail.com>
2026-04-23 09:58:41 +0200

    Update Main.kt

--- Papadustream/src/main/kotlin/Main.kt
+++ Papadustream/src/main/kotlin/Main.kt
@@ -1,8 +1,11 @@
 import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.TvType
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 
+@CloudstreamPlugin
 class Papadustream : MainAPI() {
     override var name = "Papadustream"
     override var mainUrl = "https://papadustream.singles"
+    override var lang = "fr"
     override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
 }
