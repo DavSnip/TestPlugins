1a00b37c79ee8080c97f4855986c65401e05ff65
DavSnip <davidconeggo@gmail.com>
2026-04-23 08:44:24 +0200

    Update Main.kt

--- Flemmix/Main.kt
+++ Flemmix/Main.kt
@@ -1,7 +1,7 @@
 import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.TvType
 
-class Papadustream : MainAPI() {
+class Flemmix : MainAPI() {
     override var name = "Flemmix"
     override var mainUrl = "https://flemmix.wales"
     override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
