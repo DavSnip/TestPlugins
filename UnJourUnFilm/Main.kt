b4365e000af51b85e4b52fdbda2d053b1dcf1251
DavSnip <davidconeggo@gmail.com>
2026-04-23 08:43:35 +0200

    Update Main.kt

--- UnJourUnFilm/Main.kt
+++ UnJourUnFilm/Main.kt
@@ -1,7 +1,7 @@
 import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.TvType
 
-class Papadustream : MainAPI() {
+class UnjourUnfilm : MainAPI() {
     override var name = "UnjourUnfilm"
     override var mainUrl = "https://1jour1film0426c.site"
     override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
